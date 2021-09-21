2306bbfe7a3ab8963e4b403f4b4e71acac693a99
onlis <aleksei@onl.is>
2021-09-21 21:18:45 +0500

    create: yandex map api connected

--- js/main.js
+++ js/main.js
@@ -12,4 +12,14 @@ const swiper = new Swiper('.swiper', {
         onlyInViewport: false,
       },
   });
-  
+  ymaps.ready(init);
+  function init(){
+      var myMap = new ymaps.Map("map", {center: [7.890730, 98.294727], zoom: 17});
+      var myGeoObject = new ymaps.GeoObject({
+        geometry: {
+            type: "Point", // тип геометрии - точка
+            coordinates: [7.890730, 98.294727] // координаты точки
+        }
+      });
+      myMap.geoObjects.add(myGeoObject); 
+  }
